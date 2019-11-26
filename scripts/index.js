f6e6914b05c2b60d3aa24b956ed052cf978e9940
Ajschlander <Aschlander21@gmail.com>
2019-11-25 22:39:18 -0600

    Added localstorage

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,22 @@
 const searchBtn = document.getElementById('search-button');
 const currentWeatherDiv = document.getElementById('current-weather-div');
-const forecastWeatherDiv = document.getElementById('forecast-we');
+const searchHistoryDiv = document.getElementById('search-history-div');
+const forecastWeatherDiv = document.getElementById('forecast-weather-div');
+const pastSearchElements = document.getElementsByClassName("past-search");
 let cityInput = document.getElementById('search-input');
+
+let pastSearches = [];
 
 
 const APIkey = "ff74d3c9ca5275d0eae234af4f58526d";
 let queryURL;
 
+for (let i = 0; i < array.length; i++) {
+   
+}
+
 const getCurrentWeather = () => {
+  clearSearchHistory();
   cityInput = document.getElementById('search-input').value;
   queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=" + APIkey;
 
@@ -57,6 +66,8 @@ const getCurrentWeather = () => {
     });
   });
   getForecast();
+  updateSearchHistory(cityInput);
+  populateSearchHistory();
 }
 
 const clearCurrentWeather = () => {
@@ -72,11 +83,31 @@ const getForecast = () => {
   .then(function(response){
     console.log(response.data);
     // Create a new div with a styling class and append it to the forecast div
+    const forecastDataDiv = document.createElement("div");
+    
   });
 }
 
-const updateSearchHistory = () => {
+const updateSearchHistory = (cityTitle) => {
+  pastSearches.push(cityTitle);
+  window.localStorage.setItem("history", JSON.stringify(pastSearches));
+  console.log(JSON.parse(localStorage.getItem("history")));
+}
 
+const populateSearchHistory = () => {
+  const storedSearches = JSON.parse(localStorage.getItem("history"));
+  for (let i = 0; i < storedSearches.length; i++) {
+    const newH3 = document.createElement("h3");
+    newH3.innerHTML = storedSearches[i];
+    newH3.classList.add("past-search");
+    searchHistoryDiv.prepend(newH3);
+  }
+}
+
+const clearSearchHistory = () => {
+  while (searchHistoryDiv.firstChild) {
+    searchHistoryDiv.removeChild(searchHistoryDiv.firstChild);
+  }
 }
 
 searchBtn.addEventListener("click", getCurrentWeather);
