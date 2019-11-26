5aae2ea9c9cea936e9d0a754d9c211cc9288812e
Ajschlander <Aschlander21@gmail.com>
2019-11-26 17:11:08 -0600

    Finished functionality

--- scripts/index.js
+++ scripts/index.js
@@ -11,10 +11,6 @@ let pastSearches = [];
 const APIkey = "ff74d3c9ca5275d0eae234af4f58526d";
 let queryURL;
 
-for (let i = 0; i < array.length; i++) {
-   
-}
-
 const getCurrentWeather = () => {
   clearSearchHistory();
   cityInput = document.getElementById('search-input').value;
@@ -23,15 +19,18 @@ const getCurrentWeather = () => {
   axios.get(queryURL)
   .then(function(response){
     clearCurrentWeather();
+    clearForecastDiv();
     console.log(response.data);
     // Grab the weather icon and set it to a variable
     // Create an h3 with a class of "city-name" and add it to the div of current weather with the name of the city
     const newH3 = document.createElement("h3");
     newH3.classList.add("city-name");
     newH3.innerHTML = response.data.name;
+    const currentDate = moment().format('L');
+    newH3.append(" | ", currentDate);
     const imgIcon = document.createElement("img");
     const iconcode = response.data.weather[0].icon;
-    var iconurl = "https://openweathermap.org/img/w/" + iconcode + ".png";
+    const iconurl = "https://openweathermap.org/img/w/" + iconcode + ".png";
     imgIcon.setAttribute("src", iconurl);
     newH3.append(imgIcon);
     currentWeatherDiv.append(newH3);
@@ -65,9 +64,10 @@ const getCurrentWeather = () => {
       currentWeatherDiv.append(uvIndexP);
     });
   });
-  getForecast();
+  getForecast(cityInput);
   updateSearchHistory(cityInput);
   populateSearchHistory();
+  addClickListeners();
 }
 
 const clearCurrentWeather = () => {
@@ -77,14 +77,47 @@ const clearCurrentWeather = () => {
   cityInput = document.getElementById('search-input').value = "";
 }
 
-const getForecast = () => {
-  queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityInput + "&appid=" + APIkey;
+const clearForecastDiv = () => {
+  while (forecastWeatherDiv.firstChild) {
+    forecastWeatherDiv.removeChild(forecastWeatherDiv.firstChild);
+  }
+}
+
+const getForecast = (cityName) => {
+  queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + ",us&mode=json&appid=" + APIkey;
   axios.get(queryURL)
   .then(function(response){
     console.log(response.data);
     // Create a new div with a styling class and append it to the forecast div
-    const forecastDataDiv = document.createElement("div");
-    
+    for (let i = 4; i < response.data.list.length; i+=8) {
+      console.log(response.data.list[i]);
+      // Create new card with forecast data
+      const forecastDataDiv = document.createElement("div");
+      forecastDataDiv.classList.add("forecast-card");
+
+      const forecastDate = response.data.list[i].dt_txt;
+      const forecastDateP = document.createElement("p");
+      forecastDateP.innerHTML = forecastDate;
+      forecastDataDiv.append(forecastDateP);
+      const imgIcon = document.createElement("img");
+      const iconcode = response.data.list[i].weather[0].icon;
+      const iconurl = "https://openweathermap.org/img/w/" + iconcode + ".png";
+      imgIcon.setAttribute("src", iconurl);
+      forecastDataDiv.append(imgIcon);
+      const forecastTemp = response.data.list[i].main.temp;
+      const temperatureFahrenheit = (response.data.list[i].main.temp - 273.15) * 1.80 + 32;
+      const forecastTempP = document.createElement("p");
+      forecastTempP.innerHTML = "Temperature: " + Math.round(temperatureFahrenheit) + "°F";
+      forecastDataDiv.append(forecastTempP);
+      const forecastHumidity = response.data.list[i].main.humidity;
+      const forecastHumidityP = document.createElement("p");
+      forecastHumidityP.innerHTML = "Humidity: " + forecastHumidity + "%";
+      forecastDataDiv.append(forecastHumidityP);
+
+      
+
+      forecastWeatherDiv.append(forecastDataDiv);
+    }
   });
 }
 
@@ -108,6 +141,17 @@ const clearSearchHistory = () => {
   while (searchHistoryDiv.firstChild) {
     searchHistoryDiv.removeChild(searchHistoryDiv.firstChild);
   }
+}
+
+const addClickListeners = () => {
+  const searchHistoryElements = document.querySelectorAll(".past-search");
+  for (let i = 0; i < searchHistoryElements.length; i++) {
+    const searchHistoryResult = searchHistoryElements[i];
+    searchHistoryResult.addEventListener("click", function () {
+      
+    });
+  }
+  return cityInput;
 }
 
 searchBtn.addEventListener("click", getCurrentWeather);
